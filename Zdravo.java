79a374665627ca03d8df5da74125749b33935362
Damjan <dpavlica@itekako.com>
2016-03-28 10:35:03 +0200

    m

---
+++ Zdravo.java
@@ -0,0 +1,16 @@
+class Zdravo {
+
+    public static void main(String args[]){
+     System.out.println("Zdravo Javo");
+   } // main
+
+} // Zdravo
+
+
+class Dovidjenja {
+
+    public static void main(String args[]){
+     System.out.println("Dovidjenja Javo");
+   } // main
+
+} // Dovidjenja
